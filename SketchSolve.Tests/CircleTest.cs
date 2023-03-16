42144bd861e560441fa7e7ae12774195dfb4dd18
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-03-16 11:02:09 +0000

    - check results

--- SketchSolve.Tests/CircleTest.cs
+++ SketchSolve.Tests/CircleTest.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿namespace SketchSolve.Tests;
 
-namespace SketchSolve.Tests;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
 
 [TestFixture]
 public class CircleTest
@@ -8,10 +10,19 @@ public class CircleTest
   [Test]
   public static void CenterToTest()
   {
-    var c = new Circle() { center = new Point(0, 0), rad = new Parameter(10) };
+    var circle = new Circle
+    {
+      center = new Point(0, 0),
+      rad = new Parameter(10)
+    };
     var line = new Line(new Point(-10, -10), new Point(-10, 10));
-    Console.WriteLine(line);
-    var line2 = c.CenterTo(line);
-    Console.WriteLine(line2);
+
+    var line2 = circle.CenterTo(line);
+
+    using (new AssertionScope())
+    {
+      line2.p1.x.Value.Should().Be(0);
+      line2.p1.y.Value.Should().Be(0);
+    }
   }
 }
