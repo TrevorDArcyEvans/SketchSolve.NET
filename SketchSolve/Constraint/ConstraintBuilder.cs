ce67a5ac52540c221b4557e499bb1794743f5858
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-03-17 09:11:33 +0000

    - use expression bodies to save space

--- SketchSolve/Constraint/ConstraintBuilder.cs
+++ SketchSolve/Constraint/ConstraintBuilder.cs
@@ -4,38 +4,11 @@ using SketchSolve.Model;
 
 public static class ConstraintBuilder
 {
-  public static Constraint IsHorizontal(this Line line)
-  {
-    return new HorizontalConstraint(line);
-  }
-
-  public static Constraint IsVertical(this Line line)
-  {
-    return new VerticalConstraint(line);
-  }
-
-  public static Constraint IsCoincidentWith(this Point pt, Point other)
-  {
-    return new PointOnPointConstraint(pt, other);
-  }
-
-  public static Constraint HasInternalAngle(this Line line, Line other, Parameter angle)
-  {
-    return new InternalAngleConstraint(line, other, angle);
-  }
-
-  public static Constraint HasExternalAngle(this Line line, Line other, Parameter angle)
-  {
-    return new ExternalAngleConstraint(line, other, angle);
-  }
-
-  public static Constraint IsPerpendicularTo(this Line line, Line other)
-  {
-    return new PerpendicularConstraint(line, other);
-  }
-
-  public static Constraint IsTangentTo(this Line line, Circle other)
-  {
-    return new TangentToCircleConstraint(line, other);
-  }
+  public static Constraint IsHorizontal(this Line line) => new HorizontalConstraint(line);
+  public static Constraint IsVertical(this Line line) => new VerticalConstraint(line);
+  public static Constraint IsCoincidentWith(this Point pt, Point other) => new PointOnPointConstraint(pt, other);
+  public static Constraint HasInternalAngle(this Line line, Line other, Parameter angle) => new InternalAngleConstraint(line, other, angle);
+  public static Constraint HasExternalAngle(this Line line, Line other, Parameter angle) => new ExternalAngleConstraint(line, other, angle);
+  public static Constraint IsPerpendicularTo(this Line line, Line other) => new PerpendicularConstraint(line, other);
+  public static Constraint IsTangentTo(this Line line, Circle other) => new TangentToCircleConstraint(line, other);
 }
