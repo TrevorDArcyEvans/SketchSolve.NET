2b5acc3cbf46eddab61a6ad1fbd84797226f29b1
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-03-17 09:11:15 +0000

    - added authors

--- SketchSolve/Properties/AssemblyInfo.cs
+++ SketchSolve/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("bradphelan")]
+[assembly: AssemblyCopyright("Jonathan George, bradphelan, Trevor D'Arcy-Evans")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
