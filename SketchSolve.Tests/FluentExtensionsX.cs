f8d95349c0884ca77e68b8aed1dbd01f00c480d8
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-03-17 08:54:17 +0000

    - code cleanup

--- SketchSolve.Tests/FluentExtensionsX.cs
+++ SketchSolve.Tests/FluentExtensionsX.cs
@@ -6,8 +6,8 @@ using FluentAssertions.Numeric;
 public static class FluentExtensionsX
 {
   public static AndConstraint<NumericAssertions<double>> BeApproximately
-    (this NumericAssertions<double> This, double val, double eps)
+    (this NumericAssertions<double> number, double val, double eps)
   {
-    return This.BeInRange(val - eps / 2, val + eps / 2);
+    return number.BeInRange(val - eps / 2, val + eps / 2);
   }
 }
