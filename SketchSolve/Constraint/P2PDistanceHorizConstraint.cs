e0df2534df7b149cfad243edbaf7e1acef6c84c6
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-03-28 23:01:48 +0100

    - fix calculation of error

--- SketchSolve/Constraint/P2PDistanceHorizConstraint.cs
+++ SketchSolve/Constraint/P2PDistanceHorizConstraint.cs
@@ -7,7 +7,7 @@ public sealed class P2PDistanceHorizConstraint : BaseConstraint
   public readonly Point Point1;
   public readonly Point Point2;
   public readonly Parameter Distance;
-  public override IEnumerable<object> Items => new[] { Point1, Point2 };
+  public override IEnumerable<object> Items => new[] {Point1, Point2};
 
   public P2PDistanceHorizConstraint(Point point1, Point point2, Parameter distance)
   {
@@ -18,10 +18,10 @@ public sealed class P2PDistanceHorizConstraint : BaseConstraint
 
   public override double CalculateError()
   {
-    var p1X = Point1.X.Value;
-    var p2X = Point2.X.Value;
-    var distance = Distance.Value;
-    return (p1X - p2X) * (p1X - p2X) - distance * distance;
+    var dX = Math.Abs(Point1.X.Value - Point2.X.Value);
+    var dist = Distance.Value;
+    var err = dX - dist;
+    return err * err;
   }
 
   protected override IEnumerable<IEnumerable<Parameter>> GetParameters()
